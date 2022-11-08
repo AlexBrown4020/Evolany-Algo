26e0860e31dc49bd3f25f33a3d0a9d375d1c39a6
Alexander Brown <balex3497@gmail.com>
2022-11-08 09:17:06 +0900

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,18 +1,18 @@
 const generateFile = (mb) => {
-    let kiloBytes = mb * 1024;
-    let bytes = kiloBytes * 1024;
+    let bytes = (1024 * 1024) * mb;
     let text = ''
     for (let i = 1; i <= bytes; i++) {
+        if (text.length > bytes) {
+            break;
+        }
         let hex = i.toString(16)
         while (hex.length < 8) {
             hex = '0' + hex;
         }
             text += hex;
     }
-    console.log(text)
-
-    let uriContent = URL.createObjectURL(new Blob([text], {type: 'text/plain'}));
     let element= document.createElement('a');
+    let uriContent = URL.createObjectURL(new Blob([text], {type: 'text/plain'}));
     element.setAttribute('href', uriContent);
     element.setAttribute('download', 'HexNumbers');
     element.style.display = 'none';
@@ -20,5 +20,3 @@ const generateFile = (mb) => {
     element.click();
     document.body.removeChild(element);
 }
-
-generateFile(1)
